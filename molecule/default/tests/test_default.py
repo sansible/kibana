42d848799fc3afce61cba7f11458ae353e35b47b
David Reed <david.reedfrancis@sainsburys.co.uk>
2019-01-09 14:02:13 +0000

    [GITHUB-19] Fixes download SNI errors
    Fixes SNI errors due to Elastic fiddling with their certs and
    domains.
    
    Also brought in latest repo fixes:
    
    * Latest Ansible versions
    * Pegged Python deps
    * Ansible Galaxy cert fixes
    * Ansible deprecation warnings

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,10 +5,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_users(host):
-    assert host.user('elasticsearch').group == 'elasticsearch'
-
-
 def test_listening(host):
     assert host.socket('tcp://0.0.0.0:5601').is_listening
-    assert host.socket('tcp://127.0.0.1:9200').is_listening
